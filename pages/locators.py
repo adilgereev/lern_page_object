a3d1c1b3e16060a9b9379d88d7bdc815f53ded3f
adilgereev <57193512+adilgereev@users.noreply.github.com>
2020-06-03 18:18:13 +0300

    Добавлены новые селекторы и методы для проверки формы регастрации и логина

--- pages/locators.py
+++ pages/locators.py
@@ -2,4 +2,12 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocators:
+    USER_NAME = (By.CSS_SELECTOR, '#id_login-username')
+    PASSWORD = (By.CSS_SELECTOR, '#id_login-password')
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    REGISTRATION_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
+    REPEAT_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password2')
