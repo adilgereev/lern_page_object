e78659942a3c6175fe054352b486d84f5fd0e238
adilgereev <57193512+adilgereev@users.noreply.github.com>
2020-06-05 20:46:29 +0300

    Добавлен новый файл с тестом добавления товара в корзину и проверкой совпадения названия товара и цены в отображаемом сообщении после  добавлении товара

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,11 @@ class LoginPageLocators:
     REGISTRATION_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
     REGISTRATION_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
     REPEAT_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password2')
+
+
+class ProductPageLocators:
+    BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    MESSAGE_PRODUCT = (By.CSS_SELECTOR, '#messages > div:nth-child(1) strong')
+    MESSAGE_PRICE = (By.CSS_SELECTOR, '#messages .alert-info strong')
---
+++ pages/product_page.py
@@ -0,0 +1,16 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        self.browser.find_element(*ProductPageLocators.BTN_ADD_TO_BASKET).click()
+
+    def should_be_product_added_basket(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message_product = self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT).text
+        assert product_name == message_product, 'Product name does not match the name in basket'
+
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        message_price = self.browser.find_element(*ProductPageLocators.MESSAGE_PRICE).text
+        assert product_price == message_price, 'Product price does not match the price in basket'
